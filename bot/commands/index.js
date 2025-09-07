46f0dc57385da671814886bdc1f86bf4d34e3dcb
mindbreakfast <homegamego@gmail.com>
2025-09-07 19:33:11 +0500

    Create index.js

---
+++ bot/commands/index.js
@@ -0,0 +1,11 @@
+const adminCommands = require('./adminCommands');
+const casinoCommands = require('./casinoCommands');
+const userCommands = require('./userCommands');
+const referralCommands = require('./referralCommands');
+
+module.exports = {
+    ...adminCommands,
+    ...casinoCommands, 
+    ...userCommands,
+    ...referralCommands
+};
