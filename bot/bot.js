037e1c4683c490c4c873f3d6022f542739f95920
mindbreakfast <homegamego@gmail.com>
2025-09-07 19:52:01 +0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('../config');
 const database = require('../database/database');
-const commandHandlers = require('./commands');
+const commandHandlers = require('./commands'); 
 
 const bot = new TelegramBot(config.BOT_TOKEN, { polling: false });
 console.log('Bot instance created');
