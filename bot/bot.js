f84e2be485518cb4b0521e4d00b19deff07903ca
mindbreakfast <homegamego@gmail.com>
2025-09-14 12:58:04 +0500

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const path = require('path');
-const config = require(path.join(__dirname, '..', 'config'));
+const config = require(path.join(__dirname, '..', '..', 'config'));
 const { casinoEditingState, clearUserState } = require(path.join(__dirname, 'state'));
 const commandHandlers = require(path.join(__dirname, 'commands'));
-const logger = require(path.join(__dirname, '..', '..', 'utils', 'logger'));
+const logger = require(path.join(__dirname, '..', 'utils', 'logger'));
 
 // СОЗДАЕМ бота БЕЗ автоматического запуска
 const bot = new TelegramBot(config.BOT_TOKEN, { 
