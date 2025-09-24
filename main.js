6b873d7a8ba092b0ae15137643596ca54649c899
mindbreakfast <homegamego@gmail.com>
2025-09-24 22:47:46 +0500

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+       res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Expose-Headers', 'Content-Length, Content-Range');
     next();
 });
 app.options('*', (req, res) => res.sendStatus(200));
